d1c1744ec26f4c909b06605ba6d1a129885973c0
Mandresy Gaëtan RANDRIANANTOANINA <mandresy2020@gmail.com>
2026-08-15 16:53:50 +0300

    fix: format code

--- src/main/java/com/example/demo/exception/ErrorResponse.java
+++ src/main/java/com/example/demo/exception/ErrorResponse.java
@@ -2,4 +2,5 @@ package com.example.demo.exception;
 
 import java.time.Instant;
 
-public record ErrorResponse(Instant timestamp, int status, String error, String message, String path) {}
+public record ErrorResponse(
+    Instant timestamp, int status, String error, String message, String path) {}
