13c32852556102298364d95e95051ac223b203fd
Mandresy Gaëtan RANDRIANANTOANINA <mandresy2020@gmail.com>
2026-08-15 12:53:55 +0300

    feat: add all mappers

---
+++ src/main/java/com/example/demo/mapper/CourseOfferingMapper.java
@@ -0,0 +1,33 @@
+package com.example.demo.mapper;
+
+import com.example.demo.entity.JCourseOffering;
+import com.example.demo.model.CourseOffering;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Component;
+
+@Component
+@AllArgsConstructor
+public class CourseOfferingMapper {
+
+    private final CourseMapper courseMapper;
+    private final AcademicYearMapper academicYearMapper;
+    private final GroupMapper groupMapper;
+
+    public CourseOffering toModel(JCourseOffering entity) {
+        return CourseOffering.builder()
+                .id(entity.getId())
+                .course(courseMapper.toModel(entity.getCourse()))
+                .academicYear(academicYearMapper.toModel(entity.getAcademicYear()))
+                .group(groupMapper.toModel(entity.getGroup()))
+                .build();
+    }
+
+    public JCourseOffering toEntity(CourseOffering model) {
+        return JCourseOffering.builder()
+                .id(model.id())
+                .course(courseMapper.toEntity(model.course()))
+                .academicYear(academicYearMapper.toEntity(model.academicYear()))
+                .group(groupMapper.toEntity(model.group()))
+                .build();
+    }
+}
---
+++ src/main/java/com/example/demo/mapper/GroupMapper.java
@@ -0,0 +1,23 @@
+package com.example.demo.mapper;
+
+import com.example.demo.entity.JGroup;
+import com.example.demo.model.Group;
+import org.springframework.stereotype.Component;
+
+@Component
+public class GroupMapper {
+
+    public Group toModel(JGroup entity) {
+        return Group.builder()
+                .id(entity.getId())
+                .reference(entity.getReference())
+                .build();
+    }
+
+    public JGroup toEntity(Group model) {
+        return JGroup.builder()
+                .id(model.id())
+                .reference(model.reference())
+                .build();
+    }
+}
