e583632bcb3c6614acffbb11d18b7350f17bc57a
Mandresy Gaëtan RANDRIANANTOANINA <mandresy2020@gmail.com>
2026-08-15 21:25:18 +0300

    feat: add migration db files

---
+++ src/main/resources/db/migration/V42_8__Create_enrollments_table.sql
@@ -0,0 +1,10 @@
+create table if not exists enrollments
+(
+    id uuid constraint enrollments_pk primary key,
+    student_id uuid not null constraint enrollments_student_fk references users,
+    group_id uuid not null constraint enrollments_group_fk references groups,
+    academic_year_id uuid not null constraint enrollments_academic_year_fk references academic_years,
+    level varchar not null constraint enrollments_level_check check (level in ('L1', 'L2', 'L3')),
+    start_date date not null,
+    end_date date
+);
