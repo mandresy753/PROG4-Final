0f7d347e20b21177b2c3b00ee7de67d6c71d849a
Mandresy Gaëtan RANDRIANANTOANINA <mandresy2020@gmail.com>
2026-08-15 16:58:21 +0300

    chore: remove useless comment

--- src/main/java/com/example/demo/exception/ResourceNotFoundException.java
+++ src/main/java/com/example/demo/exception/ResourceNotFoundException.java
@@ -3,7 +3,6 @@ package com.example.demo.exception;
 import java.util.UUID;
 import org.springframework.http.HttpStatus;
 
-/** Levee quand une entite identifiee par son id n'existe pas. Traduite en 404. */
 public class ResourceNotFoundException extends ApiException {
 
   public ResourceNotFoundException(String message) {
