1bdc6e6b49bcbd5515730afd40451e24a8907578
Mandresy Gaëtan RANDRIANANTOANINA <mandresy2020@gmail.com>
2026-08-16 00:37:32 +0300

    fix: correct wrong import

--- src/main/java/com/example/demo/endpoint/rest/controller/GraduationController.java
+++ src/main/java/com/example/demo/endpoint/rest/controller/GraduationController.java
@@ -1,7 +1,7 @@
 package com.example.demo.endpoint.rest.controller;
 
 import com.example.demo.enums.Track;
-import com.example.demo.model.report.Graduate;
+import com.example.demo.model.Graduate;
 import com.example.demo.service.GraduationService;
 import java.util.List;
 import lombok.AllArgsConstructor;
