fbefb34e205bbb1b3d509da2620ede3d21c62a97
Mandresy Gaëtan RANDRIANANTOANINA <mandresy2020@gmail.com>
2026-08-16 15:19:36 +0300

    feat: add database migration flyway

---
+++ src/main/resources/db/migration/V1_5__Create_course_offerings_table.sql
@@ -0,0 +1,12 @@
+create table if not exists course_offerings
+(
+    id                uuid
+        constraint course_offerings_pk primary key,
+    course_id         uuid not null
+        constraint course_offerings_course_fk references courses,
+    academic_year_id  uuid not null
+        constraint course_offerings_academic_year_fk references academic_years,
+    group_id          uuid not null
+        constraint course_offerings_group_fk references groups,
+    constraint course_offerings_course_year_group_uk unique (course_id, academic_year_id, group_id)
+);
